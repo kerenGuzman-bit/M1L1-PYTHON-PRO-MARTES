678b807820f61c46dd76d8ecc60120771eec3465
kerenGuzman-bit <kguzmany@ucentral.edu.co>
2025-12-02 19:11:38 -0500

    Add dictionary for meme definitions in main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            'Antuviar': 'Antuviar es una palabra antigua.'
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(meme_dict[word])
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
+    print("Todavía no tenemos esta palabra... Pero estamos trabajando en ella.")
+
+
